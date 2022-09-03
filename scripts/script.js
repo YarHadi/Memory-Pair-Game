074017a6fbe83dc449e8bb2d2301542a69914520
YarHadi <gadionenko8@gmail.com>
2022-09-03 17:11:44 +0300

    almost done

--- scripts/script.js
+++ scripts/script.js
@@ -1,101 +1,137 @@
 //arr of characters
 
-const characters=[
+const characters = [
     {
-        id:1,
-        img:"./img/dio.webp"
+      id: 1,
+      check: 1,
+      img: "./img/dio.webp",
     },
     {
-        id:2,
-        img:"./img/the_world.jpg"
+      id: 2,
+      check: 1,
+      img: "./img/the_world.jpg",
     },
     {
-        id:3,
-        img:"./img/giorno.webp"
+      id: 3,
+      check: 2,
+      img: "./img/giorno.webp",
     },
     {
-        id:4,
-        img:"./img/gold_exp.webp"
+      id: 4,
+      check: 2,
+      img: "./img/gold_exp.webp",
     },
     {
-        id:5,
-        img:"./img/josuke.png"
+      id: 5,
+      check: 3,
+      img: "./img/josuke.png",
     },
     {
-        id:6,
-        img:"./img/crazy_diamond.jpg"
+      id: 6,
+      check: 3,
+      img: "./img/crazy_diamond.jpg",
     },
     {
-        id:7,
-        img:"./img/jotaro.webp"
+      id: 7,
+      check: 4,
+      img: "./img/jotaro.webp",
     },
     {
-        id:8,
-        img:"./img/star_platinum.webp"
+      id: 8,
+      check: 4,
+      img: "./img/star_platinum.webp",
     },
     {
-        id:9,
-        img:"./img/kira.webp"
+      id: 9,
+      check: 5,
+      img: "./img/kira.webp",
     },
     {
-        id:10,
-        img:"./img/killer_queen.webp"
+      id: 10,
+      check: 5,
+      img: "./img/killer_queen.webp",
     },
     {
-        id:11,
-        img:"./img/jolyne.webp"
+      id: 11,
+      check: 6,
+      img: "./img/jolyne.webp",
     },
     {
-        id:12,
-        img:"./img/stone_free.webp"
+      id: 12,
+      check: 6,
+      img: "./img/stone_free.webp",
     },
-
-];
-
-//creacting field
-
-const mainField = document.querySelector(".main-field");
-
-function createMainField(data) {
-  const cards = data
-    .map(
-      (character) => `<div class="flip-container" ontouchstart="this.classList.toggle('hover');">
-      <div id="${character.id}" class="flipper">
-          <div data-id="${character.id}" class="front">
-              <img data-id="${character.id}" class="character-img" src="./img/logo.png">
-          </div>
-          <div class="back">
-              <img class="character-img" src="${character.img}">
-          </div>
-      </div>
-  </div>`
-    )
-    .join("");
+  ];
+  
+  //creacting field for a game
+  
+  const mainField = document.querySelector(".main-field");
+  
+  function createMainField(data) {
+    const cards = data
+      .map(
+        (
+          character
+        ) => `<div class="flip-container" ontouchstart="this.classList.toggle('hover');">
+        <div id="${character.id}" class="flipper">
+            <div data-id="${character.id}" class="front">
+                <img data-id="${character.id}" class="character-img" src="./img/logo.png">
+            </div>
+            <div class="back">
+                <img class="character-img" src="${character.img}">
+            </div>
+        </div>
+    </div>`
+      )
+      .join("");
     mainField.innerHTML = cards;
-}
-
-createMainField(characters);
-
-//flip func
-
-function flip(id){
-    const card=document.getElementById(id);
-    card.style.transform='rotateY(180deg)';
-}
-
-//event listener
-
-const onClick = ({ target }) => {
+  }
+  
+  createMainField(characters);
+  
+  //flip func
+  
+  function flip(id) {
+    const card = document.getElementById(id);
+    card.style.transform = "rotateY(180deg)";
+  }
+  
+  //check
+  
+  var checkElements = [];
+  
+  function checkCard(arr) {
+    const card1Id = document.getElementById(arr[0]);
+    const card1check = arr[1];
+    const card2Id = document.getElementById(arr[2]);
+    const card2check = arr[3];
+    if (card1check == card2check) {
+      checkElements = [];
+    } else {
+      checkElements = [];
+      setTimeout(() => {
+        card1Id.style.transform = "rotateY(0deg)";
+        card2Id.style.transform = "rotateY(0deg)";
+      }, 500);
+    }
+  }
+  
+  //event listener
+  
+  const onClick = ({ target }) => {
     const selectedCharacter = target.dataset.id;
-    const {id} = characters.find((character) => character.id == selectedCharacter);
-    flip(id);
+    const { id, check } = characters.find(
+      (character) => character.id == selectedCharacter
+    );
+    if (checkElements.length < 4) {
+      flip(id);
+      checkElements.push(id, check);
+    }
+    if (checkElements.length == 4) {
+      checkCard(checkElements);
+    }
   };
   
   mainField.addEventListener("click", onClick);
-
-//reset
-
-  function reset(id){
-    const card=document.getElementById(id);
-    card.style.transform='rotateY(0deg)';
-  }
+  
+  
